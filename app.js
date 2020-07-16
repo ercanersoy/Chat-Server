a4692281e34288310121ca0e46e4efe24c03ba98
Ercan Ersoy <ercanersoy@ercanersoy.net>
2020-07-16 17:41:18 +0300

    Some updates
    * Add displaying starting messages feature to console
    * Use const instead of let for socket object
    * Add displaying sended messages to browser feature
    
    Signed-off-by: Ercan Ersoy <ercanersoy@ercanersoy.net>

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 'use strict';
 
+console.log('Chat Server');
+console.log('Copyright (C) 2020 Ercan Ersoy (https://ercanersoy.net)');
+console.log('This software has licensed MIT License.');
+console.log();
+
 let express = require('express');
 let http = require('http');
 let socket_io = require('socket.io');
@@ -19,7 +24,8 @@ io.on('connection', (socket) => {
     socket.on('chat message', function(message) {
         io.emit('chat message', message);
         console.log(message);
-    })
+        socket.emit('messages', message);
+    });
 });
 
 http_server.listen(8080);
